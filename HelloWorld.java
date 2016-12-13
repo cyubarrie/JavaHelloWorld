2cc7ebf3770db3b15ed355d43585f075558443c0
Yu <yu0888@saskpolytech.ca>
2016-12-13 10:57:15 -0600

    Hello world complete

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,6 +11,7 @@ package person;
  */
 public class HelloWorld {
         public static void main(String[] args) {
-            System.out.println("Hello World");
+            //System.out.println("Hello World");
+			System.out.println("Hello brave new world");
         }
 }
